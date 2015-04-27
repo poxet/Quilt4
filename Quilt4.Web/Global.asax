2032849092bf6d7a8141a0a35f60a6d890e95e85
Daniel Bohlin <daniel.bohlin@gmail.com>
2015-04-27 22:00:39 +0200

    Added new web project

---
+++ Quilt4.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Quilt4.Web.MvcApplication" Language="C#" %>
---
+++ Quilt4.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Quilt4.Web.Startup))]
+namespace Quilt4.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
