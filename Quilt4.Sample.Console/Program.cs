63ade3566ea3f1d2316e6ec024a537c784fa6969
Daniel Bohlin <daniel.bohlin@gmail.com>
2015-05-01 19:30:10 +0200

    Added a sample client console application

---
+++ Quilt4.Sample.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using Tharga.Quilt4Net;
+
+namespace Quilt4.Sample.Console
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            System.Threading.Thread.Sleep(2000);
+
+            Configuration.ClientToken = "???";
+            Configuration.Target.Location = "http://localhost:54942/";
+
+            Session.Register();
+            Issue.Register("abc", Issue.MessageIssueLevel.Information);
+            Session.End();
+
+            System.Console.WriteLine("Press any key to exit...");
+            System.Console.ReadKey();
+        }
+    }
+}
