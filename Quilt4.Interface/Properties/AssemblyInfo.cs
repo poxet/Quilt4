defed3747f2cfcd1b63d1ac858356f5539dc5ee1
Daniel Bohlin <daniel.bohlin@gmail.com>
2015-06-01 22:58:40 +0200

    Updated build information

--- Quilt4.Interface/Properties/AssemblyInfo.cs
+++ Quilt4.Interface/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d487e9d7-c196-44e9-9b3b-21f9cf2d3271")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
