0429d7477737b55af8fea8de72079da59816056b
Pelle <pellebranden@me.com>
2015-06-13 17:59:11 +0200

    Added extension for menu item

---
+++ Quilt4.Web/Extensions/MenuExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Quilt4.Web
+{
+    public static class MenuExtensions
+    {
+        public static MvcHtmlString MenuItem(
+            this HtmlHelper<dynamic> htmlHelper,
+            string text,
+            string iconClass,
+            string action,
+            string controller
+        )
+        {
+            var active = false;
+
+            var urlHelper = new UrlHelper(htmlHelper.ViewContext.RequestContext);
+            var a = new TagBuilder("li");
+            var routeData = htmlHelper.ViewContext.RouteData;
+            var currentAction = routeData.GetRequiredString("action");
+            var currentController = routeData.GetRequiredString("controller");
+            if (string.Equals(currentAction, action, StringComparison.OrdinalIgnoreCase) &&
+                string.Equals(currentController, controller, StringComparison.OrdinalIgnoreCase))
+            {
+                active = true;
+            }
+            
+            a.InnerHtml = "<a href=\"" + urlHelper.Action(action, controller) + "\" "+ (active ? "class=\"active\"" : "") +"><i class=\"" + iconClass + "\"></i> " + text + "</a>";
+            return MvcHtmlString.Create(a.ToString());
+        }
+    }
+}
