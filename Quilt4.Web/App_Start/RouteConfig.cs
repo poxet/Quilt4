4767dcf4af9840aa0e56d7b3e3f8bc17775a2da3
Daniel Bohlin <daniel.bohlin@gmail.com>
2015-06-19 11:10:36 +0200

    Fixed routing from initiative page to application

--- Quilt4.Web/App_Start/RouteConfig.cs
+++ Quilt4.Web/App_Start/RouteConfig.cs
@@ -10,6 +10,16 @@ namespace Quilt4.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "application",
+                url: "{controller}/{action}/{id}/{application}",
+                defaults: new
+                {
+                    application = UrlParameter.Optional,
+                },
+                namespaces: new[] { "Quilt4.Web.Controllers" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new
@@ -17,16 +27,6 @@ namespace Quilt4.Web
                     controller = "Home",
                     action = "Index",
                     id = UrlParameter.Optional
-                },
-                namespaces: new[] { "Quilt4.Web.Controllers" }
-            );
-
-            routes.MapRoute(
-                name: "application",
-                url: "{controller}/{action}/{id}/{application}",
-                defaults: new
-                {
-                    application = UrlParameter.Optional,
                 },
                 namespaces: new[] { "Quilt4.Web.Controllers" }
             );
