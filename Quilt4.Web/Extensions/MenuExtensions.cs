0ce344b7a172ecbb43918e5a0f643ace39fffce8
Pelle <pellebranden@me.com>
2015-06-20 21:23:16 +0200

    Added menu extension for multi level menus

--- Quilt4.Web/Extensions/MenuExtensions.cs
+++ Quilt4.Web/Extensions/MenuExtensions.cs
@@ -7,6 +7,20 @@ using System.Web.Mvc.Html;
 
 namespace Quilt4.Web
 {
+    public class Link
+    {
+        public string Text { get; private set; }
+        public string Action { get; private set; }
+        public string Controller { get; private set; }
+
+        public Link(string text, string action, string controller)
+        {
+            Text = text;
+            Action = action;
+            Controller = controller;
+        }
+    }
+
     public static class MenuExtensions
     {
         public static MvcHtmlString MenuItem(
@@ -29,9 +43,57 @@ namespace Quilt4.Web
             {
                 active = true;
             }
-            
-            a.InnerHtml = "<a href=\"" + urlHelper.Action(action, controller) + "\" "+ (active ? "class=\"active\"" : "") +"><i class=\"" + iconClass + "\"></i> " + text + "</a>";
+
+            a.InnerHtml = "<a href=\"" + urlHelper.Action(action, controller) + "\" " + (active ? "class=\"active\"" : "") + "><i class=\"" + iconClass + "\"></i> " + text + "</a>";
             return MvcHtmlString.Create(a.ToString());
         }
+
+        
+
+        public static MvcHtmlString MenuItem(
+            this HtmlHelper<dynamic> htmlHelper,
+            string text,
+            string iconClass,
+            IEnumerable<Link> links
+        )
+        {
+
+            var urlHelper = new UrlHelper(htmlHelper.ViewContext.RequestContext);
+            var a = new TagBuilder("li");
+            var routeData = htmlHelper.ViewContext.RouteData;
+            
+            var currentAction = routeData.GetRequiredString("action");
+            var currentController = routeData.GetRequiredString("controller");
+
+            var htmlText = "<a href=\"#\"><i class=\"" + iconClass + "\"></i> Configuration<span class=\"fa arrow\"></span></a><ul class=\"nav nav-second-level\">";
+
+            var anyActive = false;
+
+            foreach (var link in links)
+            {
+                var active = false;
+
+                if (string.Equals(currentAction, link.Action, StringComparison.OrdinalIgnoreCase) &&
+                   string.Equals(currentController, link.Controller, StringComparison.OrdinalIgnoreCase))
+                {
+                    active = true;
+                    anyActive = true;
+                }
+
+                htmlText += "<li><a href=\"" + urlHelper.Action(link.Action, link.Controller) +"\"" + (active ? "class=\"active\"" : "") + ">" + link.Text + "</a></li>";
+            }
+
+            if(anyActive)
+                a.AddCssClass("active");
+
+            htmlText += "</ul>";
+
+            a.InnerHtml = htmlText;
+
+            return MvcHtmlString.Create(a.ToString());
+        }
+
+        
+
     }
 }
