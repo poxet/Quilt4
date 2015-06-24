cefc37d3ea2e996abeffbc163334f34bd4d20a50
Jonas0 <jonas_strom_seez@hotmail.com>
2015-06-24 18:08:46 +0200

    Added details on Issue type and Version.

--- Quilt4.Web/Controllers/IssueTypeController.cs
+++ Quilt4.Web/Controllers/IssueTypeController.cs
@@ -30,14 +30,15 @@ namespace Quilt4.Web.Controllers
         // GET: IssueType/Details/5
         public ActionResult Details(string id, string application, string version, string issueType)
         {
-            var initiative = _initiativeBusiness.GetInitiatives().Single(x => x.Name == id);
-            var applicationId = initiative.ApplicationGroups.SelectMany(x => x.Applications).Single(x => x.Name == application).Id;
-            var ver = _applicationVersionBusiness.GetApplicationVersions(applicationId).Single(x => x.Version == version);
-
+            var initiative = _initiativeBusiness.GetInitiatives().SingleOrDefault(x => x.Name == id);
+            var applicationId = initiative.ApplicationGroups.SelectMany(x => x.Applications).SingleOrDefault(x => x.Name == application).Id;
+            //TODO: version is an uniqe something ... how should this be solved?
+            var ver = _applicationVersionBusiness.GetApplicationVersions(applicationId).SingleOrDefault(x => x.Version == version);
+            
             var model = new IssueTypeModel
             {
                 IssueType = ver.IssueTypes.Single(x => x.Ticket.ToString() == issueType), 
-                Sessions = _sessionBusiness.GetSessionsForApplicationVersion(application)
+                Sessions = _sessionBusiness.GetSessionsForApplicationVersion(ver.Id)
             };
             model.Users = model.Sessions.Select(user => _userBusiness.GetUser(user.UserFingerprint)).ToList();
             
