4ed442b357003f1957931d20d218b9db28cf9b25
Jonathan <jonathan.engqvist@gmail.com>
2015-06-25 13:38:58 +0200

    fix for ci db

--- Quilt4.MongoDBRepository/MongoRepository.cs
+++ Quilt4.MongoDBRepository/MongoRepository.cs
@@ -238,7 +238,7 @@ namespace Quilt4.MongoDBRepository
                         _database.GetCollection("Setting").CreateIndex(new IndexKeysBuilder().Ascending("Name"), IndexOptions.SetUnique(true));
                         _database.GetCollection("Initiative").CreateIndex(new IndexKeysBuilder().Ascending("ClientToken"), IndexOptions.SetUnique(true));
                         _database.GetCollection("AspNetUsers").CreateIndex(new IndexKeysBuilder().Ascending("UserName"), IndexOptions.SetUnique(true));
-                        _database.GetCollection("AspNetUsers").CreateIndex(new IndexKeysBuilder().Ascending("Email"), IndexOptions.SetUnique(true));
+                        //_database.GetCollection("AspNetUsers").CreateIndex(new IndexKeysBuilder().Ascending("Email"), IndexOptions.SetUnique(true));
                     }
                 }
 
