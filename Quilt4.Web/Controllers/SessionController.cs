d84dbaf18623653c00c73a2bdf99acf050408f02
Jonas0 <jonas_strom_seez@hotmail.com>
2015-07-01 17:01:19 +0200

    Added session detail page.

---
+++ Quilt4.Web/Controllers/SessionController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Quilt4.Interface;
+
+namespace Quilt4.Web.Controllers
+{
+    public class SessionController : Controller
+    {
+        private readonly ISessionBusiness _sessionBusiness;
+        private readonly IApplicationVersionBusiness _applicationVersionBusiness;
+
+        public SessionController(ISessionBusiness sessionBusiness, IApplicationVersionBusiness applicationVersionBusiness)
+        {
+            _sessionBusiness = sessionBusiness;
+            _applicationVersionBusiness = applicationVersionBusiness;
+        }
+
+        // GET: Session
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(string applicationVersionId, string sessionId)
+        {
+            var session = _sessionBusiness.GetSessionsForApplicationVersion(applicationVersionId).Single(x => x.Id == Guid.Parse(sessionId));
+            var version = _applicationVersionBusiness.GetApplicationVersions(session.ApplicationId).Single(x => x.Id == applicationVersionId);
+            
+            
+            return View(version.IssueTypes);
+        }
+    }
+}
