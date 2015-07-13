6f4af2c15c135278abc9afb9a512ad458fa25258
Jonas0 <jonas_strom_seez@hotmail.com>
2015-07-13 15:20:00 +0200

    Added filter for Issue level on version detail page.

--- Quilt4.Web/Scripts/toggleShowEnvironment.js
+++ Quilt4.Web/Scripts/toggleShowEnvironment.js
@@ -12,6 +12,19 @@
     }
 }
 
+function toggleShowIssueLevel(level) {
+    var rows = document.getElementsByClassName(level);
+
+    for (var i = 0; i < rows.length; i++) {
+        if (rows[i].parentNode.parentNode.style.display == "table-row") {
+            rows[i].parentNode.parentNode.style.display = "none";
+
+        } else {
+            rows[i].parentNode.parentNode.style.display = "table-row";
+        }
+    }
+}
+
 function changeButtonStyle(id) {
 
     if (document.getElementById(id + "LegendButton").style.border == "4px solid rgb(255, 255, 255)") {
