dcd2bee53f8678436e824cd06dddb28a3ad5e03c
Jonathan <jonathan.engqvist@gmail.com>
2015-07-20 14:37:03 +0200

    removed unused property

--- Quilt4.MongoDBRepository/Entities/UserPropertiesPersist.cs
+++ Quilt4.MongoDBRepository/Entities/UserPropertiesPersist.cs
@@ -1,15 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Quilt4.MongoDBRepository.Entities
 {
-    class UserPropertiesPersist
+    class UserPropertiesPersist : ISupportInitialize
     {
         public string Id { get; internal set; }
-        public string Username { get; internal set; }
         public IDictionary<string, string> EnvironmentColors { get; internal set; }
+
+        [BsonExtraElements]
+        private IDictionary<string, object> ExtraElements { get; set; }
+
+        public void BeginInit()
+        {
+        }
+
+        public void EndInit()
+        {
+            if (ExtraElements != null)
+            {
+                if (ExtraElements.ContainsKey("Username"))
+                {
+                    ExtraElements.Remove("Username");
+                }
+            }
+        }
     }
 }
